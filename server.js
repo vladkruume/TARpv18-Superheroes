8b33c15cd78adbb21d342b097a255500710e01d6
Vlad Kruume <Vladkruume5565@outlook.com>
2020-04-19 18:58:56 +0300

    TARpe18 Superheroes

---
+++ server.js
@@ -0,0 +1,6 @@
+ var superheroues = require('superheroes');
+ var MySuperheroName = superheroues.random();
+ console.log(MySuperheroName);
+ const supervillains = require('supervillains');
+ supervillains.random();
+ console.log(supervillains);
